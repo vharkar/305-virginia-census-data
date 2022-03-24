764091b43bb41f13575b72f2221ab98fb1058867
vharikar <vibha_harikar@intuit.com>
2022-03-24 18:06:15 +0000

    Changed zoom

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def display_results1(state, attribute):
                                     marker_opacity=0.5,
                                     marker_line_width=0))
     fig.update_layout(mapbox_style="carto-positron",
-                      mapbox_zoom=5.8,
+                      mapbox_zoom=3.0,
                       mapbox_center = {"lat": 37.0902, "lon": -95.7129})
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
